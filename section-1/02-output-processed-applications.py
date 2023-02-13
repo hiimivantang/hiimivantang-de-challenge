be6917584073e08bba541f9d517586298b9535a7
hiimivantang <hiimivantang>
2023-02-13 05:25:58 +0000

    overwrite csv if exists

--- section-1/02-output-processed-applications.py
+++ section-1/02-output-processed-applications.py
@@ -1,11 +1,11 @@
 # Databricks notebook source
 successful_applications = df = table('gold_successful_applications')
-successful_applications.write.format('csv').option('header',True).save("/mnt/hiimivantang-private-bucket/csv/successful_applications/successful_applications.csv")
+successful_applications.write.format('csv').mode('overwrite').option('header',True).save("/mnt/hiimivantang-private-bucket/csv/successful_applications/successful_applications.csv")
 
 # COMMAND ----------
 
 failed_applications = df = table('gold_unsuccessful_applications')
-failed_applications.write.format('csv').option('header',True).save("/mnt/hiimivantang-private-bucket/csv/failed_applications/failed_applications.csv")
+failed_applications.write.format('csv').mode('overwrite').option('header',True).save("/mnt/hiimivantang-private-bucket/csv/failed_applications/failed_applications.csv")
 
 # COMMAND ----------
 
