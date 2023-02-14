0ad4f5d653ac5ccb9c87de849f7cb911c790a36b
hiimivantang <hiimivantang@gmail.com>
2023-02-14 09:11:16 +0800

    section 2: modified ddl_statements.sql to include users and groups creation, and granting of necessary privileges on a need-to-have basis

--- section-2/ddl_statements.sql
+++ section-2/ddl_statements.sql
@@ -1,4 +1,17 @@
-create table if not exists items (
+create SCHEMA acme;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA acme TO postgres;
+
+CREATE USER keefer WITH PASSWORD 'password';
+CREATE USER lucas WITH PASSWORD 'password';
+CREATE USER maxi WITH PASSWORD 'password';
+
+CREATE GROUP logistics WITH USER keefer;
+CREATE GROUP analytics WITH USER lucas;
+CREATE GROUP sales WITH USER maxi;
+
+
+create table if not exists acme.items (
   id SERIAL primary key,
   name VARCHAR(100),
   manufacturer_name VARCHAR(100),
@@ -7,8 +20,7 @@ create table if not exists items (
 );
 
 
-
-create table if not exists customers (
+create table if not exists acme.customers (
   id VARCHAR(106) primary key,
   first_name VARCHAR(100) not null,
   last_name VARCHAR(100) not null,
@@ -18,19 +30,21 @@ create table if not exists customers (
 );
 
 
-
-create table if not exists sales (
+create table if not exists acme.sales (
   id SERIAL primary key,
   order_date timestamp not null, 
   fulfilled boolean default false
 );
 
 
-
-create table if not exists sales_details (
+create table if not exists acme.sales_details (
   id SERIAL primary key,
-  membership_id VARCHAR(106) references customers,
-  sales_id integer references sales,
-  item_id integer references items,
+  membership_id VARCHAR(106) references acme.customers,
+  sales_id integer references acme.sales,
+  item_id integer references acme.items,
   quantity integer not null
 );
+
+GRANT INSERT, UPDATE ON acme.sales, acme.sales_details TO logistics;
+GRANT INSERT, UPDATE on acme.items TO sales;
+GRANT SELECT ON ALL TABLES IN SCHEMA acme TO logistics, analytics, sales;
